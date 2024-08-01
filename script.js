6973e970604df033bfcdadbdf178ad7973d7cd47
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-01 10:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+
+let priceElements =document.querySelectorAll('[data-ns-test="prices"]');
+
+let total = 0;
+for (let i = 0; i < priceElement.length;i++){
+	total+= parseFloat(priceElements[i].textContent)
+};
+
+let newRow = document.createElement('tr');
+let newCell = document.createElement('td');
+
+newCell.textContent = total;
+newRow.appendChild(newCell);
+
+document.getElementById('myTable').appendChild(newRow);
+
+newCell.setAttribute('data-ns-test', 'grandTotal')
